b397e84b2e9c4c472555ad7c6143a981421f20b7
Lautaro Meza <79202133+LautaroXDS@users.noreply.github.com>
2025-09-25 11:00:19 -0300

    Update prueba descuentosprod.js

--- descuentosprod.js
+++ descuentosprod.js
@@ -12,7 +12,7 @@ productos.forEach(product => {
   if (isNaN(priceNum)) return;
 
   // Calcular precio transferencia
-  const transferencia = priceNum / 2; // <-- tu lógica
+  const transferencia = priceNum * 0.75; // <-- tu lógica
 
   // Crear un <div> para mostrarlo
   const info = document.createElement("div");
@@ -106,3 +106,4 @@ product.forEach(product => {
     media.insertAdjacentElement("afterend", info);
   }
 });
+
